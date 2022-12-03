1bd37f021a199441d7ee795145da1069b331fef8
Christoph Gierling <christoph.gierling@rewe-digital.com>
2022-12-03 11:34:50 +0100

    Add day 3 parser

--- src/test/kotlin/day3/day3.kt
+++ src/test/kotlin/day3/day3.kt
@@ -4,23 +4,53 @@ import day3.Parser.parse
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
-typealias ChangeMe = Int
 
+// ----------------------------------------------------
+// data
+// ----------------------------------------------------
+typealias Rucksack = Pair<String, String>
+
+
+// ----------------------------------------------------
+// algo
+// ----------------------------------------------------
 object Parser {
-    fun parse(input: String): ChangeMe {
-        TODO("Not yet implemented")
-    }
+    fun parse(input: String): List<Rucksack> =
+        input.lines().map {
+            val first = it.substring(0, it.length / 2)
+            val second = it.substring(it.length / 2, it.length)
+            first to second
+        }
 }
 
-fun process(parsed: ChangeMe): ChangeMe {
+// ----------------------------------------------------
+// tests
+// ----------------------------------------------------
+fun process(rucksacks: List<Rucksack>): Any {
     TODO("Not yet implemented")
 }
 
+
 class Day3Tests {
     @Test
-    internal fun `test first`() {
-       val inputSmall = ""
-       val parsed : ChangeMe= parse(inputSmall)
+    fun `test first`() {
+        val inputSmall = """
+           vJrwpWtwJgWrhcsFMMfFFhFp
+           jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+           PmmdzqPrVvPwwTWBwg
+           wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+           ttgJtRGJQctTZtZT
+           CrZsJsPPZsGzwwsLwLmpwMDw
+       """.trimIndent()
+        val parsed: List<Rucksack> = parse(inputSmall)
+        parsed shouldBe listOf(
+            "vJrwpWtwJgWr" to "hcsFMMfFFhFp",
+            "jqHRNqRjqzjGDLGL" to "rsFMfFZSrLrFZsSL",
+            "PmmdzqPrV" to "vPwwTWBwg",
+            "wMqvLMZHhHMvwLH" to "jbvcjnnSBnvTQFn",
+            "ttgJtRGJ" to "QctTZtZT",
+            "CrZsJsPPZsGz" to "wwsLwLmpwMDw",
+        )
         val result = process(parsed)
         result shouldBe 1
     }
